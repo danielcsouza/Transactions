366152e0fc4e202e10e8b8d061d00d7e96f61f02
Daniel Carlos <daniel.carlos.souza@gmail.com>
2022-05-19 17:20:15 -0300

    Destination nao obrigatorio

--- Transactions/Persistence/ViewModels/CreateAccountViewModel.cs
+++ Transactions/Persistence/ViewModels/CreateAccountViewModel.cs
@@ -4,7 +4,7 @@
     {
         public string Type { get; set; }
         public string? Origin { get; set; }
-        public string Destination { get; set; }
+        public string? Destination { get; set; }
         public double Amount { get; set; }
         
     }
